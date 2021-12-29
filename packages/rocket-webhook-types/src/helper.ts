50c35fe3b0b8bef7c5d11a4b65576ba928b13ddd
alvaroloes <alvaro@booster.cloud>
2021-12-29 18:13:37 +0000

    Remove base URL when parsing it

--- packages/rocket-webhook-types/src/helper.ts
+++ packages/rocket-webhook-types/src/helper.ts
@@ -3,7 +3,7 @@ import { WebhookRequest } from './webhook-request'
 export class Helper {
   public static getOriginFromRequest(request: WebhookRequest): string {
     const url = require('url')
-    const parsedUrl = new url.URL(request.req.url, `http://${request.req.headers.host}/`)
+    const parsedUrl = new url.URL(request.req.url)
     const pathName = parsedUrl.pathname.replace('/', '')
     const paths = pathName.split('/')
     if (paths && paths.length > 0) {
