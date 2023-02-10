8960004266ea3d7fce17d49efb1c03bbcd337094
gmarcos1 <gonzalo_marcos@optum.com>
2023-02-10 09:18:18 +0000

    Add functionID as a request field when calling to BoosterRocketDispatcher on Local Provider

--- packages/rocket-webhook-local-infrastructure/src/infra.ts
+++ packages/rocket-webhook-local-infrastructure/src/infra.ts
@@ -1,11 +1,10 @@
-import { WebhookParams, functionID, WebhookParamsEvent } from '@boostercloud/rocket-webhook-types'
-import { BoosterConfig, rocketFunctionIDEnvVar } from '@boostercloud/framework-types'
+import { WebhookParams, WebhookParamsEvent } from '@boostercloud/rocket-webhook-types'
+import { BoosterConfig } from '@boostercloud/framework-types'
 import { Router } from 'express'
 import { WebhookController } from './controllers/webhook-controller'
 
 export class Infra {
   public static mountStack(params: WebhookParams, config: BoosterConfig, router: Router): void {
-    process.env[rocketFunctionIDEnvVar] = functionID
     params.forEach((parameter: WebhookParamsEvent) =>
       router.use('/webhook', new WebhookController(parameter.origin).router)
     )
