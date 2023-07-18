ccf563ce8d6082c2cc18497ca6a7c32c8dbc299d
gmarcos1 <gonzalo_marcos@optum.com>
2023-07-18 10:02:21 +0100

    use Booster auth methods

--- packages/rocket-webhook-core/src/webhook-authorizer-builder.ts
+++ packages/rocket-webhook-core/src/webhook-authorizer-builder.ts
@@ -1,35 +1,17 @@
-import { Class, UserEnvelope, RoleInterface, NotAuthorizedError } from '@boostercloud/framework-types'
+import { Class, RoleInterface } from '@boostercloud/framework-types'
 import { WebhookAuthorizer } from '@boostercloud/rocket-webhook-types'
+import { BoosterAuthorizer } from '@boostercloud/framework-core/dist/booster-authorizer'
 
 export class WebhookAuthorizerBuilder {
-  public static allowAccess(): Promise<void> {
-    return Promise.resolve()
-  }
-
-  public static denyAccess(): Promise<void> {
-    return Promise.reject(new NotAuthorizedError('Access denied for this resource'))
-  }
-
-  public static authorizeRoles(authorizedRoles: Array<Class<RoleInterface>>, user?: UserEnvelope): Promise<void> {
-    if (user && userHasSomeRole(user, authorizedRoles)) {
-      return WebhookAuthorizerBuilder.allowAccess()
-    }
-    return WebhookAuthorizerBuilder.denyAccess()
-  }
-
   public static build(authorize: 'all' | Array<Class<RoleInterface>> | WebhookAuthorizer): WebhookAuthorizer {
-    let authorizer: WebhookAuthorizer = WebhookAuthorizerBuilder.denyAccess
+    let authorizer: WebhookAuthorizer = BoosterAuthorizer.denyAccess
     if (authorize === 'all') {
-      authorizer = WebhookAuthorizerBuilder.allowAccess
+      authorizer = BoosterAuthorizer.allowAccess
     } else if (Array.isArray(authorize)) {
-      authorizer = WebhookAuthorizerBuilder.authorizeRoles.bind(null, authorize)
+      authorizer = BoosterAuthorizer.authorizeRoles.bind(null, authorize)
     } else if (typeof authorize === 'function') {
       authorizer = authorize
     }
     return authorizer
   }
-}
-
-function userHasSomeRole(user: UserEnvelope, authorizedRoles: Array<Class<RoleInterface>>): boolean {
-  return authorizedRoles.some((roleClass) => user.roles?.includes(roleClass.name))
 }
